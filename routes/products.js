b4354e05c2a0c286f8cb3f660acaa7856f3238a2
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2023-10-11 21:52:14 +0500

    dynamic title

--- routes/products.js
+++ routes/products.js
@@ -2,15 +2,23 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Main | Boom Shop",
+  });
 });
 
 router.get("/products", (req, res) => {
-  res.render("products");
+  res.render("products", {
+    title: "Products | Boom Shop",
+    isProducts: true
+  });
 });
 
 router.get("/add", (req, res) => {
-  res.render("add");
+  res.render("add", {
+    title: "Add | Boom Shop",
+    isAdd: true
+  });
 });
 
 export default router;
