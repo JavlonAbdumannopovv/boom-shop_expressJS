e7adc17f946f35e8cf3ebac249215d78ff0db1d1
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2023-10-11 22:11:47 +0500

    login and register

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,29 @@
-import {Router} from "express";
+import { Router } from "express";
 
 const router = Router();
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   res.render("login", {
     title: "Login | Boom Shop",
-    isLogin: true
+    isLogin: true,
   });
-})
+});
 
 router.get("/register", (req, res) => {
   res.render("register", {
     title: "Register | Boom Shop",
-    isRegister: true
-  })
-})
+    isRegister: true,
+  });
+});
+
+router.post("/login", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 
 export default router;
